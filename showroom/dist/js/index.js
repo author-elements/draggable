e0eb0ddc8de937c008527af8e20034d3dd464614
Graham Butler <graham@grahambutler.com>
2019-07-29 20:51:05 -0700

    initial commit

---
+++ showroom/dist/js/index.js
@@ -0,0 +1,6 @@
+/**
+ * author-draggable-showroom v1.0.0 generated on Mon Jul 29 2019.
+ * Built at 20:47:34 GMT-0700 (Pacific Daylight Time)
+ * Copyright (c) 2019 Author.io
+ */
+"use strict";var Demo=new NGNX.VIEW.Registry({selector:".demo",namespace:"demo.",init:function(){console.log("works")}});
